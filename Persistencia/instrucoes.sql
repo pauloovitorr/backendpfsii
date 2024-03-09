e8be74f3af0d7f3e40c7a2d3cc6a91b8ef79e214
Paulo Vitor <paulov.pv25@gmail.com>
2024-03-09 17:59:55 -0300

    info sobre tabela disciplina

--- Persistencia/instrucoes.sql
+++ Persistencia/instrucoes.sql
@@ -2,22 +2,6 @@ CREATE DATABASE sistema;
 
 USE sistema;
 
-CREATE TABLE categoria(
-    cat_codigo INT NOT NULL AUTO_INCREMENT,
-    cat_descricao VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_categoria PRIMARY KEY(cat_codigo)
-);
-
-CREATE TABLE produto(
-    prod_codigo INT NOT NULL AUTO_INCREMENT,
-    prod_descricao VARCHAR(100) NOT NULL,
-    prod_precoCusto DECIMAL(10,2) NOT NULL DEFAULT 0,
-    prod_precoVenda DECIMAL(10,2) NOT NULL DEFAULT 0,
-    prod_dataValidade DATE,
-    prod_qtdEstoque DECIMAL(10,2) NOT NULL DEFAULT 0,
-    cat_codigo INT NOT NULL,
-    CONSTRAINT pk_produto PRIMARY KEY(prod_codigo),
-);
 
 
 CREATE TABLE disciplina(
@@ -33,7 +17,3 @@ CREATE TABLE disciplina(
     
 )
 
-SELECT a.nome, a.cpf, a.telefone, d.nome_disciplina, d.inicio, d.termino, p.nome, p.email, xl.codigo_aluno, xl.codigo_disciplina FROM aluno a 
-INNER JOIN aluno_disciplina xl ON a.codigo = xl.codigo_aluno
-INNER JOIN disciplina d ON d.codigo = xl.codigo_disciplina
-INNER JOIN professor p ON p.codigo = d.codigo_professor ORDER BY a.nome DESC
